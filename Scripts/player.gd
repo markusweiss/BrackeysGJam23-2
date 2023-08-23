297af9d0a22edcce329bed1f236feef5c69e2afd
markusweiss <git@code71.de>
2023-08-23 09:44:23 +0200

    change navigation to a simple way

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,22 +1,18 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 200
-const ACCELERATION_SMOOTHING = 25
 
+const SPEED = 300.0
+const STOP_LEFT = 40
+const STOP_RIGHT = 280
 
-func _ready():
-	pass
-
-
-func _process(delta):
-	var movement_vector = get_movement_vector()
-	var direction = movement_vector.normalized()
-	var target_velocity = direction * MAX_SPEED
-	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
+func _physics_process(delta):
+	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	
+	if direction != 0:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 	move_and_slide()
 
-
-func get_movement_vector():
-	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	return Vector2(x_movement, y_movement)
+	position.x = clamp(position.x, STOP_LEFT, STOP_RIGHT)
