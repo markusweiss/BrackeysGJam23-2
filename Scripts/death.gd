475963923ff4050aed821391021b38bef8a9c5ee
markusweiss <git@code71.de>
2023-09-21 13:32:46 +0200

    add archivements

--- Scripts/death.gd
+++ Scripts/death.gd
@@ -10,7 +10,14 @@ func _ready():
 	
 	if(Global.deepestDive > Global.tier1):
 		$MarginContainer/VBoxContainer/PanelContainer/VBoxContainer2/AnimatedSprite2D.play("free")
+		$HBoxContainer/PanelContainer2/arch_worm.play("arch_active")
 
+	if (Global.endingDeep <= Global.minMeter or Global.minMeterArch):
+		print(Global.minMeterArch)
+		Global.minMeterArch = true
+		print(Global.minMeterArch)
+		$HBoxContainer/PanelContainer/arch_dead.play("dead_active")
+	
 func _on_quit_button_pressed():
 	get_tree().quit()
 
--- Scripts/global.gd
+++ Scripts/global.gd
@@ -5,3 +5,5 @@ var countdownTimer = 30
 var endingDeep = 0
 var deepestDive = 0
 var tier1 = 100
+var minMeterArch = false
+var minMeter = 16
